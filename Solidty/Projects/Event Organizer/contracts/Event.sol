8cb91e5dbc576b11246479033102277cc4550e9f
rohitcodism <rohitpaula18i@gmail.com>
2023-09-30 20:19:09 +0530

    do it

--- Solidty/Projects/Event Organizer/contracts/Event.sol
+++ Solidty/Projects/Event Organizer/contracts/Event.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.0;
 
 contract eventContract {
-    struct Event{
-        address Organiser;
+    struct Event {
+        address organizer;
         string name;
         uint date;
         uint price;
@@ -13,33 +13,62 @@ contract eventContract {
     }
 
     mapping(uint => Event) public eventList;
-
-    mapping(address =>mapping(uint => uint)) public tickets;
-
+    mapping(address => mapping(uint => uint)) public tickets;
     uint public nextId;
 
+    /**
+     * @dev Creates a new event by storing its details in the `eventList` mapping.
+     * It checks if the provided date is in the future and if the number of seats is greater than 10 before creating the event.
+     * @param _name The name of the event.
+     * @param _date The date of the event in Unix timestamp format.
+     * @param _price The price of each ticket for the event.
+     * @param _seats The total number of seats available for the event.
+     */
     function createEvent(string memory _name, uint _date, uint _price, uint _seats) public {
 
-        require(_date>block.timestamp, "Please choose a correct date !!!");
+        require(_date > block.timestamp, "Please choose a correct date !!!");
 
-        require(_seats>10, "Number of seats at the event must be greater than 10.");
+        require(_seats > 10, "Number of seats at the event must be greater than 10.");
 
-        eventList[nextId] = Event(msg.sender, _name, _date, _price, _seats, _seats);
+        Event memory newEvent = Event(msg.sender, _name, _date, _price, _seats, _seats);
+
+        eventList[nextId] = newEvent;
 
         nextId++;
     }
 
-        function buyTicket(uint id, uint quantity) payable public  {
-        require(eventList[id].date != 0, "Event does not exist !!");
-
-        require(eventList[id].date > block.timestamp, "Event is ended !!");
-
+    /**
+     * @dev Allows users to buy tickets for an event.
+     * @param id The ID of the event for which the user wants to buy tickets.
+     * @param quantity The number of tickets the user wants to purchase.
+     */
+    function buyTicket(uint id, uint quantity) payable public {
         Event storage _event = eventList[id];
 
-        require(msg.value == _event.price, "Ether is not enough !!");
+        require(_event.date != 0, "Event does not exist !!");
 
-        require(_event.seats >= quantity, "Seats Unavailable !!");
+        require(_event.date > block.timestamp, "Event is ended !!");
+
+        require(msg.value == _event.price * quantity, "Ether is not enough !!");
+
+        require(_event.vacantSeats >= quantity, "Seats Unavailable !!");
 
         _event.vacantSeats -= quantity;
+
+        tickets[msg.sender][id] += quantity;
+    }
+
+    function transfer(uint event_id, uint quantity, address atendee) public {
+
+        require(eventList[event_id].date != 0, "Invalid Event !!");
+
+        require(eventList[event_id].date<block.timestamp, "Invalid Event !!");
+
+        require(tickets[msg.sender][event_id]>= quantity, "Invalid quantity");
+
+        tickets[msg.sender][event_id]-= quantity;
+
+        tickets[atendee][event_id] += quantity;
+        
     }
 }
