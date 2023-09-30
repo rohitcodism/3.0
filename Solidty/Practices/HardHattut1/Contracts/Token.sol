ad88ececef9fd3e9b04b7cee2a28c26f6489c9a1
rohitcodism <rohitpaula18i@gmail.com>
2023-09-30 17:03:31 +0530

    do it

---
+++ Solidty/Practices/HardHattut1/Contracts/Token.sol
@@ -0,0 +1,28 @@
+//spdx-license-identifier: MIT
+pragma solidity 0.8.19;
+
+contract Token{
+    string public name = "hardhat_token";
+    string public symbol = "HTT";
+    uint public totalSupply = 10000;
+
+    address public owner;
+
+    mapping(address => uint) balances;
+
+    constructor(){
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address _to, uint _amount) public {
+        require(msg.sender == owner, "Only owner can transfer tokens !!");
+        require(balances[msg.sender]>=_amount, "Insufficient Balance !!!");
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+    }
+
+    function fetchBalance(address _to) public view returns(uint){
+        return balances[_to];
+    }
+}
