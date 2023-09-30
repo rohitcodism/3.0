21b107804d4147acd19d2e59005656f126f21490
rohitcodism <rohitpaula18i@gmail.com>
2023-09-30 20:33:34 +0530

    First smart contract done

--- Solidty/Projects/Event Organizer/contracts/Event.sol
+++ Solidty/Projects/Event Organizer/contracts/Event.sol
@@ -38,7 +38,7 @@ contract eventContract {
     }
 
     /**
-     * @dev Allows users to buy tickets for an event.
+     * @dev Allows users to purchase tickets for a specific event.
      * @param id The ID of the event for which the user wants to buy tickets.
      * @param quantity The number of tickets the user wants to purchase.
      */
@@ -46,29 +46,34 @@ contract eventContract {
         Event storage _event = eventList[id];
 
         require(_event.date != 0, "Event does not exist !!");
-
         require(_event.date > block.timestamp, "Event is ended !!");
-
         require(msg.value == _event.price * quantity, "Ether is not enough !!");
-
         require(_event.vacantSeats >= quantity, "Seats Unavailable !!");
 
         _event.vacantSeats -= quantity;
-
         tickets[msg.sender][id] += quantity;
     }
 
-    function transfer(uint event_id, uint quantity, address atendee) public {
-
+    /**
+     * @dev The `transfer` function allows a user to transfer tickets for a specific event to another attendee.
+     * @param event_id The ID of the event for which the tickets are being transferred.
+     * @param quantity The number of tickets being transferred.
+     * @param attendee The address of the new attendee who will receive the transferred tickets.
+     */
+    function transfer(uint event_id, uint quantity, address attendee) public {
+        // Check if the event with the given `event_id` exists and is valid
         require(eventList[event_id].date != 0, "Invalid Event !!");
 
-        require(eventList[event_id].date<block.timestamp, "Invalid Event !!");
+        // Check if the event has already occurred
+        require(eventList[event_id].date < block.timestamp, "Invalid Event !!");
 
-        require(tickets[msg.sender][event_id]>= quantity, "Invalid quantity");
+        // Check if the sender has enough tickets to transfer
+        require(tickets[msg.sender][event_id] >= quantity, "Invalid quantity");
 
-        tickets[msg.sender][event_id]-= quantity;
+        // Subtract the transferred tickets from the sender's account
+        tickets[msg.sender][event_id] -= quantity;
 
-        tickets[atendee][event_id] += quantity;
-        
+        // Add the transferred tickets to the new attendee's account
+        tickets[attendee][event_id] += quantity;
     }
 }
