b13a2c64245020c1e0c7ca5830ead244093d139b
rohitcodism <rohitpaula18i@gmail.com>
2023-10-01 18:58:09 +0530

    chai test failed

--- Solidty/Practices/HardHattut1/Tests/token.js
+++ Solidty/Practices/HardHattut1/Tests/token.js
@@ -1,19 +1,17 @@
-import chai, { expect } from "chai";
+import { ethers } from "hardhat";
+import { expect } from "chai";
 
 describe("Token", () => {
     it("Deployment should assign the total supply of tokens to the owner", async () => {
-        const[owner] = await ethers.getSigners(); // to access the owner of the contract
+        const [owner] = await ethers.getSigners();
+        console.log("Owner:", owner);
 
-        console.log("Owner : ", owner);
+        const Token = await ethers.getContractFactory("Token");
+        const hardhatToken = await Token.deploy();
 
-        const Token = await ethers.getContractFactory("Token"); //to get a instance of the contract
+        const ownerBalance = await hardhatToken.fetchBalance(owner.address);
+        console.log("Owner Balance:", ownerBalance.toString());
 
-        const hardhatToken = await Token.deploy(); // to deploy the contract
-
-        const ownerBalance = await hardhatToken.fetchBalance(owner.address); // to get the balance of the owner
-
-        console.log("Owner Balance : ", ownerBalance.toString()); // to print the balance of the owner
-
-        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance); // to check if the total supply is equal to the owner balance
+        expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
     });
 })
