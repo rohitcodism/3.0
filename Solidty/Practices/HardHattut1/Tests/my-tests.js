81443b5308bf3189b2c5a206621f0212f54fece3
rohitcodism <rohitpaula18i@gmail.com>
2023-10-02 10:36:22 +0530

    all tests passed

--- Solidty/Practices/HardHattut1/Tests/my-tests.js
+++ Solidty/Practices/HardHattut1/Tests/my-tests.js
@@ -1,6 +1,6 @@
-const { expect } = require("chai");
-const hre = require("hardhat");
-const { ethers } = require("hardhat");
+const { expect } = require("chai"); // importing the chai library
+const hre = require("hardhat"); //importing hardhat-runtime-environment
+const { ethers } = require("hardhat"); // importing ethers
 
 describe("Token", () => {
     let Token;
@@ -11,57 +11,62 @@ describe("Token", () => {
     let addrs;
 
     beforeEach(async () => {
-        Token = await ethers.getContractFactory("Token");
-        [Owner, address1, address2, ...addrs] = await ethers.getSigners(); 
-        hardhatToken = await Token.deploy();
+        Token = await ethers.getContractFactory("Token"); // creating an instance of the contract
+        [Owner, address1, address2, ...addrs] = await ethers.getSigners(); // getting the accounts
+        hardhatToken = await Token.deploy(); // deploying the contract
     })
 
+    // testing the deployment
     describe("Deployment", () => {
         it("Should set the right owner", async () => {
-            expect(await hardhatToken.owner()).to.equal(Owner.address);
+            expect(await hardhatToken.owner()).to.equal(Owner.address); // checking if the owner is set correctly
             console.log(Owner.address)
         })
 
+        // testing the initial supply
         it("Should assign the total supply of tokens to the owner", async () => {
-            const ownerBalance = await hardhatToken.fetchBalance(Owner.address);
+            const ownerBalance = await hardhatToken.fetchBalance(Owner.address); // getting the balance of the owner
 
-            expect(ownerBalance).to.equal(10000);
+            expect(ownerBalance).to.equal(10000); // checking if the balance is set correctly
         })
 
+        // testing the transfer function
         describe("Transactions", () => {
             it("Should transfer the specified amount", async() => {
-                await hardhatToken.transfer(address1.address, 10);
-                const balance1 = await hardhatToken.fetchBalance(address1.address);
-                expect(balance1).to.equal(10);
+                await hardhatToken.transfer(address1.address, 10); // transferring 10 tokens to address1
+                const balance1 = await hardhatToken.fetchBalance(address1.address); // getting the balance of address1
+                expect(balance1).to.equal(10); // checking if the balance is set correctly
 
-                await hardhatToken.transfer(address2.address, 5);
-                const balance2 = await hardhatToken.fetchBalance(address2.address);
-                expect(balance2).to.equal(5);
+                await hardhatToken.transfer(address2.address, 5); // transferring 5 tokens to address2
+                const balance2 = await hardhatToken.fetchBalance(address2.address); // getting the balance of address2
+                expect(balance2).to.equal(5); // checking if the balance is set correctly
 
-                await hardhatToken.connect(address1).transfer(address2.address, 5);
-                const balanceX = await hardhatToken.fetchBalance(address2.address);
-                expect(balanceX).to.equal(10);
+                await hardhatToken.connect(address1).transfer(address2.address, 5); // transferring 5 tokens from address1 to address2
+                const balanceX = await hardhatToken.fetchBalance(address2.address); // getting the balance of address2
+                expect(balanceX).to.equal(10); // checking if the balance is set correctly
             })
 
+            // testing the transfer function for failure
             it("Fail!! if sender doesn't have enough tokens.", async () => {
-                const initialOwnerBalance = await hardhatToken.fetchBalance(Owner.address);
-                await expect(hardhatToken.connect(address1).transfer(Owner.address, 1)).to.be.revertedWith("Insufficient Balance !!!");
-                await expect(await hardhatToken.fetchBalance(Owner.address)).to.equal(initialOwnerBalance);
+                const initialOwnerBalance = await hardhatToken.fetchBalance(Owner.address); // getting the initial balance of the owner
+                await expect(hardhatToken.connect(address1).transfer(Owner.address, 1)).to.be.revertedWith("Insufficient Balance !!!"); // checking if the transfer fails
+                await expect(await hardhatToken.fetchBalance(Owner.address)).to.equal(initialOwnerBalance); // checking if the balance is set correctly
             })
 
+            // testing the update balance function
             it("Should update balance correctly.", async () => {
-                const initialOwnerBalance = await hardhatToken.fetchBalance(Owner.address);
-                await hardhatToken.transfer(address1.address, 10);
-                await hardhatToken.transfer(address2.address, 10);
+                const initialOwnerBalance = await hardhatToken.fetchBalance(Owner.address); // getting the initial balance of the owner
+                await hardhatToken.transfer(address1.address, 10); // transferring 10 tokens to address1
+                await hardhatToken.transfer(address2.address, 10); // transferring 10 tokens to address2
 
-                const finalOwnerBalance = await hardhatToken.fetchBalance(Owner.address);
-                expect(finalOwnerBalance).to.equal(initialOwnerBalance-20);
+                const finalOwnerBalance = await hardhatToken.fetchBalance(Owner.address); // getting the final balance of the owner
+                expect(finalOwnerBalance).to.equal(initialOwnerBalance-20); // checking if the balance is set correctly
 
-                const balanceX = await hardhatToken.fetchBalance(address1.address);
-                expect(balanceX).to.equal(10);
+                const balanceX = await hardhatToken.fetchBalance(address1.address); // getting the balance of address1
+                expect(balanceX).to.equal(10); // checking if the balance is set correctly
 
-                const balanceY = await hardhatToken.fetchBalance(address2.address);
-                expect(balanceY).to.equal(10);
+                const balanceY = await hardhatToken.fetchBalance(address2.address); // getting the balance of address2
+                expect(balanceY).to.equal(10); // checking if the balance is set correctly
             })
         })
     })
