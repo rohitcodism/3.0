9f5ded2b22b504dd4d0e5c8b2c901fa81233dc97
rohitcodism <rohitpaula18i@gmail.com>
2023-10-03 19:48:39 +0530

    test 2 passed

--- Solidty/Practices/HardHattut1/Tests/school-tests.js
+++ Solidty/Practices/HardHattut1/Tests/school-tests.js
@@ -23,12 +23,13 @@ describe("SchoolData", () => {
         //testing the enlister function if it is enlisting a new student correctly
         it("Should enlist a new student.", async() => {
             const newEnlistedStudent = await hardhatSchoolData.newStudent(4, "Rohit Paul", 125, 14, "CSE3");
-            const data = hardhatSchoolData.StudentList(4);
+            const data = await hardhatSchoolData.StudentList(4);
             expect(data.name).to.equal("Rohit Paul");
             expect(data.roll_number).to.equal(125);
             expect(data.class).to.equal(14);
             expect(data.section).to.equal("CSE3")
             console.log(newEnlistedStudent);
+            console.log("Student data : ",data);
         })
     })
 })
