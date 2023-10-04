51e332456acf70d64868f7d6b0bb45461dc94c90
rohitcodism <rohitpaula18i@gmail.com>
2023-10-04 10:50:47 +0530

    Receiver function executed

--- Solidty/Projects/Lottery/contracts/Lottery.sol
+++ Solidty/Projects/Lottery/contracts/Lottery.sol
@@ -6,6 +6,16 @@ contract Lottery {
     address payable[] public players; // participants
 
     constructor() {
-        manager = msg.sender;
+        manager = msg.sender; // global variable
+    }
+
+    receive() external payable {
+        require(msg.value>=2 ether, "Transaction amount must be greater than 2 ethers.");
+        players.push(payable(msg.sender));
+    }
+
+    function getContractBalance() public view returns(uint){
+        require(msg.sender == manager, "Only manager can check the contract balance.");
+        return address(this).balance;
     }
 }
