a71dc5fe5a6ecdb7eeceb26878994b0a763a5458
rohitcodism <rohitpaula18i@gmail.com>
2023-10-06 07:07:04 +0530

    reset function added, contract compiled

--- Solidty/Projects/Lottery/contracts/Lottery.sol
+++ Solidty/Projects/Lottery/contracts/Lottery.sol
@@ -20,14 +20,14 @@ contract Lottery {
         return address(this).balance;
     }
 
-    function winnerChcker() public view returns(address){
+    function winnerChcker() public view returns(address payable){
         require(players.length >= 3, "Not enough participants.");
         uint Winner = uint256(keccak256(abi.encodePacked(block.timestamp, blockhash(block.number-1)))) % players.length;
         return players[Winner];
     }
 
     function transferPrize() public payable {
-        require(msg.sender == manager, "Only manager can transfer the prize amount.")
+        require(msg.sender == manager, "Only manager can transfer the prize amount.");
         winner = winnerChcker();
         winner.transfer(address(this).balance);
         
@@ -38,14 +38,12 @@ contract Lottery {
     }
 
     function fetchWinnerBalance() public view returns(uint){
-        require(msg.sender == winner, "Only winner can check his balance.")
+        require(msg.sender == winner, "Only winner can check his balance.");
         return winner.balance;
     }
 
     function resetLottery() public {
         require(msg.sender == manager, "Only manager can reset the lottery.");
-        while(players[players.length-1] != 0){
-            players.pop();
-        }
+        players = new address payable[](0);
     }
 }
