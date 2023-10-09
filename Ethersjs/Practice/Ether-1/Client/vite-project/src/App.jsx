677b5dff264e6ea9889a148c231f0930e78827aa
rohitcodism <rohitpaula18i@gmail.com>
2023-10-09 21:24:09 +0530

    first dApp done

--- Ethersjs/Practice/Ether-1/Client/vite-project/src/App.jsx
+++ Ethersjs/Practice/Ether-1/Client/vite-project/src/App.jsx
@@ -1,25 +1,93 @@
 
 import './App.css'
-import { useEffect } from 'react'
 import { ethers } from 'ethers'
 import { walletAddress, walletABI } from '../../../Server/interactionSC'
+import { useState } from 'react';
 function App() {
 
-  useEffect(() => {
-    const writeContract = async () => {
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      await provider.send(`eth_requestAccounts`,[]);
-      const signer = await provider.getSigner();
-      const contract = new ethers.Contract(walletAddress, walletABI, signer);
-      const Value = await contract.setValue(3);
+
+  const [clicked, setClicked] = useState(false);
+
+  const [uClicked, setUClicked] = useState(false);
+
+  const [balance, setBalance] = useState(0);
+
+  const [address, setAddress] = useState(``);
+
+  const [userAddress, setUserAddress] = useState(``);
+
+  const [userBalance, setUserBalance] = useState(0);
+
+
+
+  const writeContract = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    await provider.send(`eth_requestAccounts`, []);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(walletAddress, walletABI, signer);
+    const valueC = await contract.getBalance();
+    setBalance(valueC);
+    await contract.sendEthContract({ value: ethers.parseEther("0.02") })
+  }
+
+  const fetchContractBalance = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(walletAddress, walletABI, signer);
+    const valueC = ethers.formatEther(await contract.getBalance());
+    setBalance(valueC);
+  }
+
+  const sendEtherUser = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    await provider.send(`eth_requestAccounts`, []);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(walletAddress, walletABI, signer);
+    try{
+      await contract.sendEthUser(`${address}`, {value : ethers.parseEther("0.0005")})
+    }catch(error){
+      console.log(error);
     }
-    writeContract();
-  },[])
+  }
+
+  const fetchUserBalance = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(walletAddress, walletABI, signer);
+    const valueU = ethers.formatEther(await contract.accountBalance(userAddress));
+    console.log(valueU);
+    setUserBalance(valueU);
+  }
+
+
 
   return (
     <>
       <h1>Alphamask</h1>
-      <h3></h3>
+      <div>
+        <button onClick={() => { writeContract() }}>Send Ether to Contract</button>
+        <br />
+        {clicked ? <h3>{balance} ethers</h3> : null}
+        <br />
+        {!clicked ? <button onClick={() => { setClicked(!clicked); fetchContractBalance(); }}>Fetch contract balance</button> : <button onClick={() => {setClicked(!clicked)}}>Hide Contract Balance</button>}
+        {console.log(balance)}
+        <br />
+        <br />
+        <form>
+          <input type="text"  onChange={(e) => {setAddress(e.target.value)}}/>
+        </form>
+        <br />
+        <button onClick={() => {sendEtherUser(); document.getElementById('addr').value = "";}}>Send Ether</button>
+        <br />
+        <br />
+        <form >
+          <input type="text"  onChange={(e) => {setUserAddress(e.target.value)}}/>
+        </form>
+        <br />
+        {uClicked ? <h3>{userBalance} ethers</h3> : null}
+        <br />
+        {!uClicked ? <button onClick={() => { setUClicked(!uClicked); fetchUserBalance(); }}>Fetch User balance</button> : <button onClick={() => {setUClicked(!uClicked)}}>Hide User Balance</button>}
+      </div>
     </>
   )
 }
