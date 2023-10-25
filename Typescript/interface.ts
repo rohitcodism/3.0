b1730b583897b359e7d26ad4aa2c55177cf1a290
rohitcodism <rohitpaula18i@gmail.com>
2023-10-25 21:45:18 +0530

    more-on-interface

--- Typescript/interface.ts
+++ Typescript/interface.ts
@@ -15,6 +15,18 @@ interface User {
     getCoupon(couponCode : string): boolean; // Function property, takes a string and returns a boolean
 }
 
+//We can add properties to an interface later
+//We can't do this with a type
+interface User {
+    githubID?: string; // Optional property, can be added later
+
+}
+
+// Inheritance of interfaces
+interface specialUser extends User {
+    specialID: string;
+} // We can extend an interface
+
 // Example 2: Defining a type
 type user = {
     email: string;
@@ -28,9 +40,47 @@ const user1: User = {
     userID: 1,
     startTrial: () => "Trial started", // Function property, returns a string
     startSubscription: () => {return "Subscription started"}, // Function property, returns a string, method 2, optional so no error , it can be undefined
-    getCoupon: (couponCode: "sjk42309243@fds") => {return true}  // Function property, takes a string and returns a boolean
+    getCoupon: (couponCode: "sjk42309243@fds") => {return true},  // Function property, takes a string and returns a boolean
+    githubID: "rohit" // Optional property, no error, can be added later
+}
+
+// Example 4: Interface extending another interface
+const user2: specialUser = {
+    specialID: "123",
+    _DBid: 1543780935,
+    email: "rohit@.com",
+    userID: 1,
+    startTrial: () => "Trial started", // Function property, returns a string
+    startSubscription: () => {return "Subscription started"}, // Function property, returns a string, method 2, optional so no error , it can be undefined
+    getCoupon: (couponCode: "sjk42309243@fds") => {return true},  // Function property, takes a string and returns a boolean
 }
 
 
 user1._DBid = 123; // Error: Cannot assign to '_DBid' because it is a read-only property.
 user1.email = "rohit@coolmail.com" // No error, because email is not readonly.
+
+
+// Type v/s Interface
+
+// Type is used to define a type
+// Interface is used to define a structure
+
+// Type can be used to define a union type
+// Interface cannot be used to define a union type
+
+// Type can be used to define a tuple type
+// Interface cannot be used to define a tuple type
+
+// Type can be used to define an intersection type
+// Interface cannot be used to define an intersection type
+
+// Type can be used to define an alias
+// Interface cannot be used to define an alias
+
+// Type can be used to define a primitive type
+// Interface cannot be used to define a primitive type
+
+// Type can be used to define a function type
+// Interface cannot be used to define a function type
+
+
