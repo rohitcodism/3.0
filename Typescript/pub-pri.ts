1c32d645c32d626efb24aad6953b18b64ae273d7
rohitcodism <rohitpaula18i@gmail.com>
2023-10-26 17:11:56 +0530

    protected check

--- Typescript/pub-pri.ts
+++ Typescript/pub-pri.ts
@@ -1,5 +1,6 @@
 // All the properties that is not marked as private or public , they are always marked as public.
 class User {
+    protected nickname: string;
     public name: string;
     private age: number;
     public email: string;
@@ -26,3 +27,15 @@ class modernUser {
 }
 
 const rohit = new modernUser("Rohit Paul", "rohit@.com", "@93#8$40djf");
+
+// Inheritance, super, protected
+// super: The super keyword is used to access and call functions on an object's parent.
+// protected: The protected keyword is similar to private, except that protected members can be accessed using their deriving classes.
+class anotherTypeUser extends User {
+    githubID: string;
+    constructor(name: string, age: number, email: string, githubID: string) {
+        super(name, age, email);
+        this.githubID = githubID;
+        this.nickname = "Anu";
+    }
+}
